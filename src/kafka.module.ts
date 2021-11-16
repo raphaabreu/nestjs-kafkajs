9c8e4e0d97610ce6d6985975e95b12f38e92d420
Raphael Abreu <raphael.lorenzeto@gmail.com>
2021-11-16 14:00:02 -0300

    Initial version

---
+++ src/kafka.module.ts
@@ -0,0 +1,25 @@
+import { DynamicModule } from '@nestjs/common';
+import { Kafka, KafkaConfig } from 'kafkajs';
+import { logCreator } from './log-creator';
+
+export class KafkaModule {
+  static forRoot(config?: KafkaConfig): DynamicModule {
+    config = {
+      brokers: (process.env.KAFKA || 'localhost:9092').split(','),
+      ssl: true,
+      logCreator,
+      ...config,
+    };
+
+    return {
+      module: KafkaModule,
+      providers: [
+        {
+          provide: Kafka,
+          useFactory: () => new Kafka(config),
+        },
+      ],
+      exports: [Kafka],
+    };
+  }
+}
---
+++ src/log-creator.ts
@@ -0,0 +1,32 @@
+import { logLevel } from 'kafkajs';
+import { StructuredLogger } from '@raphaabreu/nestjs-opensearch-structured-logger';
+
+export function logCreator() {
+  const rootLogger = new StructuredLogger('Kafka');
+
+  return ({ level, log }) => {
+    const { message, timestamp, logger, ...extra } = log;
+
+    const scoppedLogger = rootLogger.createScope(extra);
+
+    if (extra.error == 'The group is rebalancing, so a rejoin is needed') {
+      level = logLevel.INFO;
+    }
+
+    switch (level) {
+      case logLevel.ERROR:
+      case logLevel.NOTHING:
+        scoppedLogger.error(message, '');
+        break;
+      case logLevel.WARN:
+        scoppedLogger.warn(message);
+        break;
+      case logLevel.INFO:
+        scoppedLogger.log(message);
+        break;
+      case logLevel.DEBUG:
+        scoppedLogger.debug(message);
+        break;
+    }
+  };
+}
